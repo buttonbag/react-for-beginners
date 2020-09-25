aeefc0f756a20af11e2dd28b7bf9f2e8174f2139
Louie Camacho <louie.camacho@hogarth-ogilvy.com>
2020-09-25 04:29:58 -0500

    create ref, push store name to url from getFunName

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+	myInput = React.createRef();
+
+	goToStore = (event) => {
+		/* prevent form from submitting */
+		event.preventDefault();
+		/* log value from input */
+		const storeName = this.myInput.current.value;
+		/* set store name to that value */
+		this.props.history.push(`/store/${storeName}`);
+	};
 	render() {
 		return (
-			<form className="store-selector">
+			<form className="store-selector" onSubmit={this.goToStore}>
 				<h2>Please Enter a Store</h2>
-				<input type="text" required placeholder="Enter Store Name" />
+				<input
+					type="text"
+					ref={this.myInput}
+					required
+					placeholder="Enter Store Name"
+					defaultValue={getFunName()}
+				/>
 				<button type="submit">Visit Store &gt;</button>
 			</form>
 		);
